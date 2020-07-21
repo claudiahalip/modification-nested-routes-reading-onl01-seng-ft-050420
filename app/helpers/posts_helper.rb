62ccd8e9b7da80b30126f60c22f1f1fd576a1f26
claudia v <claudia4ro@yahoo.com>
2020-07-21 04:59:07 +0000

    Automatically backed up by Learn

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -7,15 +7,5 @@ module PostsHelper
         hidden_field_tag "post[author_id]", post.author_id
       end
     end
-  end
-=======
-def author_id_field(post)
-    if post.author.nil?
-      select_tag "post[author_id]", options_from_collection_for_select(Author.all, :id, :name)
-    else
-      hidden_field_tag "post[author_id]", post.author_id
-    end
-  end
 end
 
->>>>>>> 652d84096d756ea6b4d2f319d471d728ff3638ec
